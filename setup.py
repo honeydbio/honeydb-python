7db4ea6af7859569d59f7b51dcb0f8f57a0331cc
foospidy <phillip@foospidy.com>
2018-02-12 08:52:20 -0500

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(HERE, 'README.rst')) as f:
 
 setup(
     name="honeydb",
-    version="0.0.2",
+    version="0.0.3",
     author="foospidy",
     description=("A python wrapper for the HoneyDB API - "
                  "https://riskdiscovery.com/honeydb/#threats"),
