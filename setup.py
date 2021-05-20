2b6ff351443fa4b0bce14d076b51fff422a0dbb9
foospidy <phillip@foospidy.com>
2021-05-20 17:03:52 -0400

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(HERE, 'README.rst')) as f:
 
 setup(
     name="honeydb",
-    version="1.3.0",
+    version="1.3.1",
     author="foospidy",
     description=("A Python API wrapper and CLI tool for the HoneyDB."),
     license="MIT",
